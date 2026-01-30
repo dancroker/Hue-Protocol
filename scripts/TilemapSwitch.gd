1ca4fed5784060c8263cae4a428d53f14e7c9733
Ju-im <joshua2.imarhiagbe@live.uwe.ac.uk>
2026-01-30 21:46:02 +0000

    added template

--- scripts/TilemapSwitch.gd
+++ scripts/TilemapSwitch.gd
@@ -1,23 +1,22 @@
 extends Node2D
 
-@onready var ground: TileMap = $Node/Ground
+#@onready var ground: TileMap = $Node/Ground
 @onready var ground_blue: TileMap = $"Node/Ground Blue"
+@onready var ground: TileMapLayer = $Node/Layer0
 
-# Called when the node enters the scene tree for the first time.
+# C$Node/Layer0alled when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#var direction := Input.is_action_pressed("Switch Mask")
+	SwitchTile()
 	pass
 	
 func SwitchTile():
-	if Input.is_action_pressed("Switch Mask"):
-		if ground.visible == false:
-			ground.visible = true
-			ground_blue.visible = false
-		elif ground.visible == true:
-			ground.visible = false
-			ground_blue.visible = true
+	if Input.is_action_just_pressed("Switch Mask"):
+		
+		ground.visible=!ground.visible
+		ground_blue.visible=!ground_blue.visible
+		
