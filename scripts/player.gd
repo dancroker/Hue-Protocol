eab0613a73ef55c01d233225a3ecf59f437dbbfd
Bubgus <toby2.still@live.uwe.ac.uk>
2026-01-31 17:03:53 +0000

    Updated Map - Dan!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 175.0
 const JUMP_VELOCITY = -600.0
 var MAX_NUM_JUMP = 1
 var jump_vairable = -100
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 		MAX_NUM_JUMP=1
 	# Handle jump.
 	if Input.is_action_pressed("ui_accept") and MAX_NUM_JUMP > 0:
-		if jump_vairable > -700:
+		if jump_vairable > -600:
 			jump_vairable -= 10
 	if Input.is_action_just_released("ui_accept") and is_on_floor():
 		MAX_NUM_JUMP -=1
