51bcf81964c7d73c087075ef355dcbb03f61a73d
Ju-im <joshua2.imarhiagbe@live.uwe.ac.uk>
2026-01-31 16:03:05 +0000

    added shaders properly

---
+++ scripts/crt_effect.gd
@@ -0,0 +1,16 @@
+extends ColorRect
+var MIN_X
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	MIN_X= global_position.x
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print(global_position.x)
+
+	pass
+	
