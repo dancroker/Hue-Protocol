757a856e3c47cfe3fa15cf6ec6044ccc04f27a39
JCUWE25 <jack3.chambers@live.uwe.ac.uk>
2026-01-31 13:58:35 +0000

    added player rigid body

---
+++ scripts/player_rb.gd
@@ -0,0 +1,31 @@
+extends RigidBody2D
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+
+const MAX_SPEED = 100.0
+const MOVE_SPEED = 100.0
+const JUMP_FORCE = -600.0
+var MAX_NUM_JUMP = 1
+var jump_vairable = -100
+var MASK_EQUIPT = 0;
+
+
+func _physics_process(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	var force = Vector2.ZERO
+
+	if direction:
+		force.x = MOVE_SPEED * direction
+		if abs(linear_velocity.x) > MAX_SPEED: 
+			linear_velocity.x = MAX_SPEED  * direction
+	if _on_floor() and Input.is_action_just_pressed("ui_accept"):
+		force.y += JUMP_FORCE
+	
+	apply_central_impulse(force)
+
+func _intergrate_forces(state):
+	rotation_degrees = 0
+
+func _on_floor():
+	if ray_cast_2d.is_colliding():
+		print("is collidongt")
+		return true
